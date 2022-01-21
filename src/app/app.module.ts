df5f41431c40c257415d08bd08737fe988898b5d
gustavorodri2011 <gustavorodri2011@gmail.com>
2022-01-20 22:42:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { ContadorModule } from './contador/contador.module';
+
+import { HeroesModule } from './heroes/heroes.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Antman'];
+  heroeBorrado: any;
+  borrarHeroe() {
+    console.log('Borrando...');
+    this.heroeBorrado = this.heroes.shift!();
+  }
+}
